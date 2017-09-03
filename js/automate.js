5ed1caf7b13086bb279c1622fa43cf85ea9687d8
K4r0qtuYNE5G4qgZ <K4r0qtuYNE5G4qgZ@users.noreply.github.com>
2017-09-03 11:43:31 +0200

    Added pattern check for Steam Key
    Allows users to manually correct it as an example for those posted on /r/FreeGameFindings/ or /r/FreeGamesOnSteam/ to avoid bots

--- js/automate.js
+++ js/automate.js
@@ -1,6 +1,16 @@
 // console.log('automate script injected');
 
-if (document.getElementById('product_key').value != '') {
-    document.getElementById('accept_ssa').click();
+var _re = new RegExp('^[0-9A-Z]{4,7}-[0-9A-Z]{4,7}-[0-9A-Z]{4,7}(?:(?:-[0-9A-Z]{4,7})?(?:-[0-9A-Z]{4,7}))?$','i');
+var _key = document.getElementById('product_key').value;
+var _valid = (_key != '') && _re.test(_key);
+
+// Click SSA checkbox
+document.getElementById('accept_ssa').click();
+
+// Only click the button if key isn't empty and match pattern to allow users to correct it manually
+// as an example for those posted on /r/FreeGameFindings/ or /r/FreeGamesOnSteam/ to avoid bots
+if (_valid) {
     document.getElementById('register_btn').click();
 }
+
+// End of file;
