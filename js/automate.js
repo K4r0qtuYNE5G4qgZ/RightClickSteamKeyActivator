c84b5908e28338361b55d644338216df031af787
Roman Davydov <15850461+rdavydov@users.noreply.github.com>
2022-06-22 14:54:45 +0300

    added 2s timeout before accept_ssa click
    Click SSA checkbox after 2s timeout (it doesn't click w/o the timeout)

--- js/automate.js
+++ js/automate.js
@@ -4,8 +4,8 @@ var _re = new RegExp('^[0-9A-Z]{4,7}-[0-9A-Z]{4,7}-[0-9A-Z]{4,7}(?:(?:-[0-9A-Z]{
 var _key = document.getElementById('product_key').value;
 var _valid = (_key != '') && _re.test(_key);
 
-// Click SSA checkbox
-document.getElementById('accept_ssa').click();
+// Click SSA checkbox after 2s timeout (it doesn't click w/o the timeout)
+setTimeout(() => { document.getElementById('accept_ssa').click(); }, 2000);
 
 // Only click the button if key isn't empty and match pattern to allow users to correct it manually
 // as an example for those posted on /r/FreeGameFindings/ or /r/FreeGamesOnSteam/ to avoid bots
