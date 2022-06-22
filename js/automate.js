84e3142c182012fb7a087643061766c87a5e900a
NiRaNTaLi <K4r0qtuYNE5G4qgZ@users.noreply.github.com>
2022-06-22 17:56:42 +0200

    Changed Auto SSA checkbox Method
    Changed the Auto SSA checkbox Method from clicking it into "checked = true" Method like the SteamDB Extensions does.
    This will solve the Issue with unchecking it again if already checked by SteamDB and similar Extensions.
    Additionally a flexible fix for timing issues was added.

--- js/automate.js
+++ js/automate.js
@@ -4,8 +4,15 @@ var _re = new RegExp('^[0-9A-Z]{4,7}-[0-9A-Z]{4,7}-[0-9A-Z]{4,7}(?:(?:-[0-9A-Z]{
 var _key = document.getElementById('product_key').value;
 var _valid = (_key != '') && _re.test(_key);
 
-// Click SSA checkbox
-document.getElementById('accept_ssa').click();
+// Check SSA checkbox
+var checkSSAExist = setInterval(function CheckSSA() {
+const element = document.getElementById( 'accept_ssa' );
+if( element )
+{
+    element.checked = true;
+    clearInterval(checkSSAExist);
+}
+}, 100); // check every 100ms
 
 // Only click the button if key isn't empty and match pattern to allow users to correct it manually
 // as an example for those posted on /r/FreeGameFindings/ or /r/FreeGamesOnSteam/ to avoid bots
