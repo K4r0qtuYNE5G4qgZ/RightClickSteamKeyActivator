05d89c6ca3830f6ae6d7e23de8c7bdb734e50e50
NiRaNTaLi <K4r0qtuYNE5G4qgZ@users.noreply.github.com>
2022-09-05 15:50:21 +0200

    Optimized Auto SSA checkbox & Continue Click (Mv3)

--- js/automate.js
+++ js/automate.js
@@ -1,23 +1,21 @@
+// The script only clicks continue if key isn't empty and match pattern to allow users to correct it manually
+// as an example for those posted on /r/FreeGameFindings/ or /r/FreeGamesOnSteam/ to avoid bots.
 // console.log('automate script injected');
 
 var _re = new RegExp('^[0-9A-Z]{4,7}-[0-9A-Z]{4,7}-[0-9A-Z]{4,7}(?:(?:-[0-9A-Z]{4,7})?(?:-[0-9A-Z]{4,7}))?$','i');
 var _key = document.getElementById('product_key').value;
 var _valid = (_key != '') && _re.test(_key);
 
-// Check SSA checkbox
+// Check SSA Checkbox and Click Continue
 var checkSSAExist = setInterval(function CheckSSA() {
-const element = document.getElementById( 'accept_ssa' );
-if( element )
-{
-    element.checked = true;
-    clearInterval(checkSSAExist);
-}
+    const element = document.getElementById('accept_ssa');
+    if (element) {
+        element.checked = true;
+        clearInterval(checkSSAExist);
+        if (_valid) {
+            document.getElementById('register_btn').click();
+        }
+    }
 }, 100); // check every 100ms
-
-// Only click the button if key isn't empty and match pattern to allow users to correct it manually
-// as an example for those posted on /r/FreeGameFindings/ or /r/FreeGamesOnSteam/ to avoid bots
-if (_valid) {
-    document.getElementById('register_btn').click();
-}
 
 // End of file;
