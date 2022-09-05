39909036dddf8e9ac3ba1c55110f5bda6d79a33c
NiRaNTaLi <K4r0qtuYNE5G4qgZ@users.noreply.github.com>
2022-09-05 15:55:30 +0200

    Add files via upload

--- _Firefox/js/skactivater.js
+++ _Firefox/js/skactivater.js
@@ -1,14 +1,17 @@
-function activatekey(info,tab) {
-  chrome.tabs.create({
-    url: "https://store.steampowered.com/account/registerkey?key=" + encodeURIComponent(info.selectionText.replace(/\s/g,''))}, function(tab) {
-    browser.tabs.executeScript(tab.id, {file: '/js/automate.js'});
-  });
+function activatekey(info, tab) {
+    chrome.tabs.create({
+        url: "https://store.steampowered.com/account/registerkey?key=" + info.selectionText.replace(/\s/g, '')
+    }, function(tab) {
+        chrome.tabs.executeScript(tab.id, {
+            file: 'js/automate.js'
+        });
+    });
 };
 
-browser.contextMenus.create({
-  title: "Activate Steam Key: %s",
-  contexts:["selection"],
-  onclick: activatekey,
+chrome.contextMenus.create({
+    title: "Activate Steam Key: %s",
+    contexts: ["selection"],
+    onclick: activatekey,
 });
 
 // End of file;
